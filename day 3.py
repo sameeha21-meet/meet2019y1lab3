b9234b420eb78f1adb20336596f4a20d5fd2f230
sameeha21-meet <sameeha21@meet.mit.edu>
2019-07-23 12:37:57 +0300

    Stopping here

---
+++ day 3.py
@@ -0,0 +1,33 @@
+respond= input("say something!")
+respond= respond.upper()
+print(respond)
+user= input('how are you')
+user= user.lower()
+print(user)
+name= input("whats up")
+name= name.swapcase()
+print(name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
---
+++ greeting.py
@@ -0,0 +1,5 @@
+user= input("whats your name")
+print(user)
+respond=input("hello there " + user.capitalize())
+print(respond)
+print ("your name is"+ str (len("sameha"))+"letters long")
